8c5f39026fe70c965bb9135f9a104ab6697db629
NZ <ns@zav.me>
2023-02-14 23:37:47 +0600

    Style organization. Adding themes

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import './styles/index.scss';
+import {useTheme} from "./theme/useTheme";
+
+const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Изменить тему</button>
+            <h1>React app</h1>
+            <Link to='/'>Главная</Link>
+            <Link to='/about'>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
