be2b0eef20d117a8f05a7bc01aaf733170002b87
NZ <ns@zav.me>
2023-03-16 23:49:31 +0600

    Non-existent routes. Loader for loading pages

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,14 +1,17 @@
 import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
+import {NotFoundPage} from "pages/NotFoundPage";
 
 export enum AppRoutes {
     MAIN = 'main',
-    ABOUT = 'about'
+    ABOUT = 'about',
+    NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
+    [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig = [
@@ -19,5 +22,9 @@ export const routeConfig = [
     {
         path: RoutePath.about,
         element: <AboutPage />,
+    },
+    {
+        path: RoutePath.not_found,
+        element: <NotFoundPage />,
     },
 ];
