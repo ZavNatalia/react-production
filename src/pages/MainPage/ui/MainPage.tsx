2fbc1d380568a3260e8525d0ddb205af460cda04
NZ <ns@zav.me>
2023-04-27 17:34:16 +0600

    add redux store, add first entity

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,7 @@
 import { BugButton } from 'app/providers/ErrorBoundary';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import Counter from '../../../entities/Counter/ui/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
@@ -8,6 +9,7 @@ const MainPage = () => {
         <div>
             <BugButton />
             <h2>{t('Main page')}</h2>
+            <Counter />
         </div>
     );
 };
--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar';
-import { ComponentRender } from 'shared/lib/tests/componentRender/ComponentRender';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('Sidebar', () => {
     test('Test Sidebar', () => {
-        ComponentRender(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('Test toggle', () => {
-        ComponentRender(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
 
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
