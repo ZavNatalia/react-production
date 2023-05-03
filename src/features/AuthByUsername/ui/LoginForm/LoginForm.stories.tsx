3b707172edb00c37e1ced236234f7696a409db40
NZ <ns@zav.me>
2023-05-03 21:56:42 +0600

    add pre commit

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -1,6 +1,5 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
-import { Input } from 'shared/ui/Input/Input';
 import { LoginForm } from './LoginForm';
 
 export default {
