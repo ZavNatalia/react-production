0d28413f9849d6a3653c166795a1c124bccebd6f
NZ <ns@zav.me>
2023-05-18 20:19:08 +0600

    add tests for login module

---
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -0,0 +1,39 @@
+import { Dispatch } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { loginByUsername } from 'features/AuthByUsername/model/services/loginByUsername/loginByUsername';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { userActions } from 'entities/User';
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
+
+jest.mock('axios');
+
+const mockedAxios = jest.mocked(axios, true);
+
+describe('loginByUsername.test', () => {
+    test('success login', async () => {
+        const userValue = { username: '123', id: '1' };
+
+        mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
+
+        const thunk = new TestAsyncThunk(loginByUsername);
+        const result = await thunk.callThunk({ username: '321', password: '123' });
+
+        expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
+        expect(thunk.dispatch).toHaveBeenCalledTimes(3);
+        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe('fulfilled');
+        expect(result.payload).toEqual(userValue);
+    });
+
+    test('error login', async () => {
+        mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
+
+        const thunk = new TestAsyncThunk(loginByUsername);
+        const result = await thunk.callThunk({ username: '654', password: '456' });
+
+        expect(thunk.dispatch).toHaveBeenCalledTimes(2);
+        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe('rejected');
+        expect(result.payload).toEqual('error');
+    });
+});
---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,20 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/LoginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSlice.test', () => {
+    test('test set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: 'John' };
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setUsername('Vasya'),
+        )).toEqual({ username: 'Vasya' });
+    });
+    test('test set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: 'qwerty' };
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setPassword('ytrewq!'),
+        )).toEqual({ password: 'ytrewq!' });
+    });
+});
