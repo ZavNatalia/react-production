86d5ebb005f3557cc15ac9007f349a4e76d2aa53
NZ <ns@zav.me>
2023-08-28 23:08:12 +0600

    add gesture, add react spring, add dynamic lib loading with AnimationProvider

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -7,6 +7,7 @@ import { NotificationList } from 'entities/Notification';
 import { Popover } from 'shared/ui/Popups';
 import { Drawer } from 'shared/ui/Drawer/Drawer';
 import { useDevice } from 'shared/lib/hooks/useDevice/useDevice';
+import { AnimationProvider } from 'shared/lib/components/AnimationProvider';
 import cls from './NotificationButton.module.scss';
 
 interface NotificationButtonProps {
@@ -35,9 +36,11 @@ export const NotificationButton = memo(({ className }: NotificationButtonProps) 
         return (
             <>
                 {trigger}
-                <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-                    <NotificationList />
-                </Drawer>
+                <AnimationProvider>
+                    <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+                        <NotificationList />
+                    </Drawer>
+                </AnimationProvider>
             </>
         );
     }
