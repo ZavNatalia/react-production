f5b921d6a770e267381585d7ad744e1d638e072a
NZ <ns@zav.me>
2023-09-07 19:32:03 +0600

    add js doc

--- src/shared/lib/hooks/useModal/useModal.tsx
+++ src/shared/lib/hooks/useModal/useModal.tsx
@@ -8,6 +8,12 @@ interface UseModalProps {
     animationDelay: number;
 }
 
+/**
+ * Переиспользуемый хук для модальных компонентов (drawer/modal)
+ * @param onClose
+ * @param isOpen
+ * @param animationDelay
+ */
 export function useModal(props: UseModalProps) {
     const {
         onClose, isOpen, animationDelay,
