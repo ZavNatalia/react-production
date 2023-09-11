5a10eec3b46aa9cd9f28d8c4f5af94cb1316b39a
NZ <ns@zav.me>
2023-09-11 23:24:00 +0600

    add helpers for slice, selectors, actions

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from 'react';
 import { Button } from '@/shared/ui/Button';
-import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
-import { counterActions } from '../model/slice/counterSlice';
+import { useCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { useCounterActions } from '../model/slice/counterSlice';
+import { HStack, VStack } from '@/shared/ui/Stack';
+import { Input } from '@/shared/ui/Input';
+import { Card } from '@/shared/ui/Card';
 
 export const Counter = () => {
-    const dispatch = useDispatch();
-    const counterValue = useSelector(getCounterValue);
-    const increment = () => {
-        dispatch(counterActions.increment());
-    };
+    const counterValue = useCounterValue();
+    const [value, setValue] = useState('');
+    const {
+        increment, decrement, add, subtract,
+    } = useCounterActions();
 
-    const decrement = () => {
-        dispatch(counterActions.decrement());
-    };
+    const handleInc = () => increment();
+
+    const handleDec = () => decrement();
+
+    const handleAdd = () => add(Number(value));
+
+    const handleSubtract = () => subtract(Number(value));
+
+    const handleInput = (v: string) => setValue(v);
 
     return (
-        <div>
-            <h1 data-testid="value-title">{counterValue}</h1>
-            <Button data-testid="decrement-button" onClick={decrement}>-1</Button>
-            <Button data-testid="increment-button" onClick={increment}>+1</Button>
-        </div>
+        <Card>
+            <VStack gap="16">
+                <h1 data-testid="value-title">{counterValue}</h1>
+                <HStack gap="8">
+                    <Button data-testid="decrement-button" onClick={handleDec}>-1</Button>
+                    <Button data-testid="increment-button" onClick={handleInc}>+1</Button>
+                </HStack>
+                <HStack gap="8">
+                    <Input onChange={handleInput} value={value} type="number" />
+                    <Button data-testid="plus-button" onClick={handleAdd}>+</Button>
+                    <Button data-testid="minus-button" onClick={handleSubtract}>-</Button>
+                </HStack>
+            </VStack>
+        </Card>
     );
 };
