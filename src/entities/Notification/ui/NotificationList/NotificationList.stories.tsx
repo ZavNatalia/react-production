48c28c3234931f4bfdb24e995d93e529a7369fac
NZ <ns@zav.me>
2023-09-15 20:50:04 +0600

    upgrade storybook, fix packages

--- src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
+++ src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import withMock from 'storybook-addon-mock';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator';
 import { NotificationList } from './NotificationList';
 
@@ -10,7 +9,6 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
-    decorators: [withMock],
 } as ComponentMeta<typeof NotificationList>;
 
 const Template: ComponentStory<typeof NotificationList> = (args) => <NotificationList {...args} />;
