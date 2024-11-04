6c9f73cca64b0c03dc2e3368a643e1e77cc2c305
NZ <ns@zav.me>
2024-11-05 01:23:44 +0300

    update the About page, add translations, update font sizes and colors

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,12 +1,73 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './AboutPage.module.scss';
 
-const AboutPage = () => {
+const AboutPage = ({ className }: { className?: string }) => {
     const { t } = useTranslation('about');
     return (
-        <Page data-testid="AboutPage">
+        <Page
+            data-testid="AboutPage"
+            className={classNames(cls.AboutPage, {}, [className])}
+        >
             <h2>{t('About us')}</h2>
+            <p>{t('The project employs')}</p>
+            <ul>
+                <li>
+                    <span>{t('React')}</span>&nbsp;
+                    {t('for building the user interface')}
+                </li>
+                <li>
+                    <span>{t('TypeScript')}</span>&nbsp;
+                    {t('for static typing')}
+                </li>
+                <li>
+                    <span>{t('Webpack')}</span>&nbsp;
+                    {t('for bundling and dependency management')}
+                </li>
+                <li>
+                    <span>{t('Cypress')}</span>&nbsp;
+                    {t('for end-to-end testing')}
+                </li>
+                <li>
+                    <span>{t('Storybook')}</span>&nbsp;
+                    {t('for documenting and testing components')}
+                </li>
+                <li>
+                    <span>{t('ESLint')}</span>&nbsp;
+                    {t('and Stylelint for code and style quality assurance')}
+                </li>
+                <li>
+                    <span>{t('Babel')}</span>&nbsp;
+                    {t('for transpiling modern JavaScript')}
+                </li>
+                <li>
+                    <span>{t('Prettier')}</span>&nbsp;
+                    {t('for code formatting')}
+                </li>
+                <li>
+                    <span>{t('Husky')}</span>&nbsp;
+                    {t('for pre-commit checks')}
+                </li>
+                <li>
+                    <span>{t('Docker')}</span>&nbsp;
+                    {t('for containerizing the application')}
+                </li>
+                <li>
+                    <span>{t('Netlify')}</span>&nbsp;
+                    {t('for deployment')}
+                </li>
+                <li>
+                    <span>{t('Jest')}</span>&nbsp;
+                    {t('for unit testing')}
+                </li>
+            </ul>
+            <p>
+                {t(
+                    'This structure provides a comprehensive approach to developing, testing, and deploying the application',
+                )}
+            </p>
         </Page>
     );
 };
